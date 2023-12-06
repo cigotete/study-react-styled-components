9c552e501436ca5b8e46f7dc9f458b5085299935
Camilo Figueroa <cigotete@gmail.com>
2023-12-06 16:04:38 -0500

    Applied global style. Added a custom font.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from '@/components/common'
+import { Button } from '@/components/common';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Caveat', cursive;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
+      <h1>App</h1>
       <Button>
-        Test
+        <h1>Test loren impsum</h1>
       </Button>
     </>
   )
