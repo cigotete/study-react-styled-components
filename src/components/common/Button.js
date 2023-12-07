557c085760caffba536fbdaf3e355f3b3e29796c
Camilo Figueroa <cigotete@gmail.com>
2023-12-07 18:44:01 -0500

    Change default color secondary button.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const Button = styled.button`
   color: white;
-  background-color: ${props => props.secondary ? 'black' : '#f8049c'};
+  background-color: ${props => props.secondary ? '#fdd54f' : '#f84904'};
   font-weight: bold;
 
   ${props => props.large ?
