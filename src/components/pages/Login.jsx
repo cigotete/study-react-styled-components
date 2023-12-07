36557282e07506e7ec85ceae626807d4b827bc43
Camilo Figueroa <cigotete@gmail.com>
2023-12-07 13:27:11 -0500

    Added form to Login page, and implemented username and password fields with associated userState hook and event handler function.

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,9 +1,38 @@
-import { PageLayout } from "@/components/common";
+import { useState } from "react";
+import { PageLayout, Input, Form } from "@/components/common";
 
 export const Login = () => {
+  const [formFields, setFormFields] = useState({username: '', password: ''});
+
+  function handleInputChange(e){
+    e.persist();
+    setFormFields(s => ({
+        ...s,
+        [e.target.name]: e.target.value
+    }));
+  }
+
   return (
     <PageLayout>
-      Login
+      <h1>Login</h1>
+      <Form>
+        <Input
+          value={ formFields.username }
+          onChange={  handleInputChange }
+          name="username"
+          type="text"
+          placeholder="Username"
+        >
+        </Input>
+        <Input
+          value={ formFields.password }
+          onChange={  handleInputChange }
+          name="password"
+          type="password"
+          placeholder="Password"
+        >
+        </Input>
+      </Form>
     </PageLayout>
   );
 }
