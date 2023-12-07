4fb41ea89c069c46a5fab2da5230fb7248d373dd
Camilo Figueroa <cigotete@gmail.com>
2023-12-07 13:11:46 -0500

    Added dummy text to Home page.

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,16 @@ import { PageLayout } from "@/components/common";
 export const Home = () => {
   return (
     <PageLayout>
-      Home
+      <h1>Home</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit justo in tortor interdum, id pulvinar risus imperdiet. Fusce porta neque et nibh eleifend, a tempor velit molestie. Duis eu justo lectus. Ut ut suscipit odio. Proin sed erat hendrerit diam rhoncus aliquet. Sed cursus metus ac felis congue, eu pulvinar orci ullamcorper. Nunc quis imperdiet enim. Integer ut magna quam. Nunc id aliquam nunc. Quisque hendrerit pulvinar lacus. Sed ornare ex nulla. Mauris vestibulum mauris vitae volutpat eleifend. Proin eu nulla id est gravida vestibulum et at lacus. Nulla quis arcu justo.
+      </p>
+      <p>
+      Aenean quis odio quis massa aliquet sodales. Fusce facilisis ornare neque, sit amet blandit mi auctor vitae. Praesent id mi pretium, blandit sapien sit amet, tincidunt urna. Curabitur sed elit nec ex congue tristique non et justo. Vestibulum pellentesque tincidunt massa, faucibus volutpat neque consectetur eu. Pellentesque lorem odio, mollis et nunc id, feugiat mattis ex. Vestibulum semper sapien et nunc maximus, quis tristique nisl luctus. Cras in dolor velit. Suspendisse ex neque, efficitur et magna eu, fermentum interdum ipsum.
+      </p>
+      <p>
+      Duis porta tincidunt sem. Sed semper leo sit amet dignissim blandit. Aenean vel pretium tellus. Pellentesque venenatis dolor sed purus laoreet viverra. Nulla luctus quis nunc eget mollis. Sed non pharetra felis, at tristique justo. In ut odio lorem. Morbi ultrices imperdiet arcu, quis iaculis diam tristique quis. Nunc congue consequat ex, sit amet auctor nibh condimentum et. Pellentesque vestibulum commodo mollis. In rutrum consequat nisi. Proin ut magna eget ligula mattis pulvinar eget in lectus.
+      </p>
     </PageLayout>
   );
 }
