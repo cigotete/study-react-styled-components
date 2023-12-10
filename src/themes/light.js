74b9b79c9645ca1184bd625dd493d98eada904a5
Camilo Figueroa <cigotete@gmail.com>
2023-12-09 21:35:18 -0500

    Created Dark and Light themes.

---
+++ src/themes/light.js
@@ -0,0 +1,11 @@
+const theme = {
+  id: 'light',
+  primaryColor: '#f84904',
+  secondaryColor: '#fdd54f',
+  bodyBackgroundColor: 'white',
+  bodyFontColor: 'black',
+  tertiaryColor: '#47597e',
+  font: 'Open Sans'
+};
+
+export default theme;
